6dd3864ff8936c14a85864a9580c5db38756e741
Kevin Flores <ftkevin18@gmail.com>
2024-09-27 20:07:00 -0500

    configuración bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
+import { ProductComponent } from './components/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"servicios",
+    component:ServicioComponent
+  },
+  {
+    path:"product",
+    component:ProductComponent
+  }
+];

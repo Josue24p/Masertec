a0766be389b6d8804d10ab1119b192e7792697b7
Lonely <josuepoza.24@gmail.com>
2025-01-09 20:09:25 -0500

    Se agregó al .ts la nueva ruta que manejara el app-root tendrá acceso al componente nuevo navegación (se hizo un componente nuevo navegación que este individual) y asimismo se agregó el routerOutlet aunque ya estaba el RouterModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent, RouterOutlet, NavegacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

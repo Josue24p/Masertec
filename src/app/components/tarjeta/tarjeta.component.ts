65537e64dc8c20d1338d4018b86fb0a2bdd4d880
Lonely <josuepoza.24@gmail.com>
2025-01-10 15:14:02 -0500

    Se modificó el componente principal tarjeta, se modificó estilos del botón para que este abajo siempre (margin-top: auto), se modificó la estructura que puede incluir contenido adicional, se adicionó nuevos inputs para manejar el estado del botón y las rutas.

--- src/app/components/tarjeta/tarjeta.component.ts
+++ src/app/components/tarjeta/tarjeta.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tarjeta',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tarjeta.component.html',
   styleUrl: './tarjeta.component.css'
 })
@@ -12,10 +13,13 @@ export class TarjetaComponent {
   @Input() title!: string;
   @Input() imageUrl!: string;
   @Input() buttonText: string = 'Ver más';
+  @Input() showButton: boolean = false;
+  @Input() route!: string;
 
   constructor(private router: Router) { }
 
   onButtonClick() {
-    this.router.navigate(['/']);
+    if (this.route)
+      this.router.navigate([this.route]);
   }
 }

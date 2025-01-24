15499203731c7a1c898640c689642f7e311be3ec
Kevin Flores <ftkevin18@gmail.com>
2025-01-24 18:31:53 -0500

    validators para el ingreso de datos en contactanos

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
-
+  formularioContacto : FormGroup
+  
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['', Validators.required],
+      celular: ['', Validators.required],
+      tipoEmail:['',[Validators.required, Validators.email]],
+      tamanio:['',[Validators.required, Validators.email]]
+    }); 
+  }
+  hasErrors(controlName: string, errorType: string){
+    return this.formularioContacto.get(controlName)?.hasError(errorType)&&
+    this.formularioContacto.get(controlName)?.touched
+   }
 }
+
